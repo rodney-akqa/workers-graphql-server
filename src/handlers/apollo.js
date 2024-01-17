8d37633e21513f04a0c1999bba711d4298f96a70
Rodney Greenfield <70877601+rodney-akqa@users.noreply.github.com>
2024-01-17 19:12:15 +1100

    Update apollo.js

--- src/handlers/apollo.js
+++ src/handlers/apollo.js
@@ -14,6 +14,38 @@ const dataSources = () => ({
 
 const kvCache = { cache: new KVCache() }
 
+const graphQLOptions = {
+  // Set the path for the GraphQL server
+  baseEndpoint: '/',
+
+  // Set the path for the GraphQL playground
+  // This option can be removed to disable the playground route
+  playgroundEndpoint: '/___graphql',
+
+  // When a request's path isn't matched, forward it to the origin
+  forwardUnmatchedRequestsToOrigin: false,
+
+  // Enable debug mode to return script errors directly in browser
+  debug: false,
+
+  // Enable CORS headers on GraphQL requests
+  // Set to `true` for defaults (see `utils/setCors`),
+  // or pass an object to configure each header
+  cors: true,
+  // cors: {
+  //   allowCredentials: 'true',
+  //   allowHeaders: 'Content-type',
+  //   allowOrigin: '*',
+  //   allowMethods: 'GET, POST, PUT',
+  // },
+
+  // Enable KV caching for external REST data source requests
+  // Note that you'll need to add a KV namespace called
+  // WORKERS_GRAPHQL_CACHE in your wrangler.toml file for this to
+  // work! See the project README for more information.
+  kvCache: false,
+}
+
 const createServer = (graphQLOptions) =>
   new ApolloServer({
     typeDefs,
@@ -26,7 +58,7 @@ const createServer = (graphQLOptions) =>
 // moved to global memory
 const server = createServer(graphQLOptions)
 
-const handler = async (request, graphQLOptions) => {
+const handler = async (request) => {
   await server.start()
   return graphqlCloudflare(() => server.createGraphQLServerOptions(request))(
     request,
